1380928d0eff30b2994366cabb1d5966a1b06813
iko <mnrhwow@gmail.com>
2023-08-02 16:42:00 +0300

    cherry-pick c082f4f "removed service locator"

--- shared/src/commonMain/kotlin/com/kopylovis/kmmcomposemuliplatformexample/core/presentation/ViewModelFactory.kt
+++ shared/src/commonMain/kotlin/com/kopylovis/kmmcomposemuliplatformexample/core/presentation/ViewModelFactory.kt
@@ -2,16 +2,24 @@ package com.kopylovis.kmmcomposemuliplatformexample.core.presentation
 
 import androidx.compose.runtime.Composable
 import com.kopylovis.kmmcomposemuliplatformexample.contacts.presentation.screens.contactlist.ContactListViewModel
-import com.kopylovis.kmmcomposemuliplatformexample.di.ServiceLocator
+import com.kopylovis.kmmcomposemuliplatformexample.utils.PlatformTestUtils
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-@Composable
-fun provideContactViewModel(key: String): ContactListViewModel = getViewModel(
-    key = key,
-    factory = viewModelFactory {
-        ContactListViewModel(
-            platformTestUtils = ServiceLocator.platformUtils
+object ViewModelFactory: KoinComponent {
+
+    @Composable
+    fun provideContactViewModel(key: String): ContactListViewModel {
+        val platformTestUtils by inject<PlatformTestUtils>()
+        return getViewModel(
+            key = key,
+            factory = viewModelFactory {
+                ContactListViewModel(
+                    platformTestUtils = platformTestUtils
+                )
+            }
         )
     }
-)
+}
